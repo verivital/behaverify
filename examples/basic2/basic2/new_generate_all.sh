4233f7b13c1439ac6d5b73668435aeedca31690f
serene <serene@penguin>
2022-06-05 09:14:13 -0600

    created cleaner double_path

--- examples/basic2/basic2/new_generate_all.sh
+++ examples/basic2/basic2/new_generate_all.sh
@@ -5,6 +5,6 @@ files=("example1" "example2" "example3" "example4" "example5" "example6" "exampl
 
 
 for cur_file in ${files[@]}; do
-    python3 behaverify_double_path.py $cur_file create_root --output_file ../models_double_path/$cur_file.smv --overwrite
+    python3 behaverify_double_path_clean.py $cur_file create_root --output_file ../models_double_path_clean/$cur_file.smv --overwrite
 done
 
