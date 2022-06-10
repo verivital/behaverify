94bb0756a3844dcf8fd19701d9fc987429ddc8b4
serene <serene@penguin>
2022-06-10 00:24:41 -0600

    total mostly working. need to run some cheks with some of the BlueROV stuff

--- examples/basic2/basic2/generate_all.sh
+++ examples/basic2/basic2/generate_all.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-files=("example1" "example2" "example3" "example4" "example5" "example6" "example7" "example8")
+files=("example0" "example1" "example2" "example3" "example4" "example5" "example6" "example7" "example8")
+versions=("total" "double_path" "double_path_clean" "double_path_cleaner" "double_path_ivar")
 
 
 for cur_file in ${files[@]}; do
-    python3 behaverify_selector.py $cur_file create_root --root_args True --output_file ../models_selector_mixed/$cur_file.smv --selector_mode 0 --overwrite
-
-    python3 behaverify_selector.py $cur_file create_root --output_file ../models_selector_parallel/$cur_file.smv --selector_mode 1 --overwrite
-
-    python3 behaverify_selector.py $cur_file create_root --output_file ../models_selector_non_parallel/$cur_file.smv --selector_mode 2 --overwrite
-
-    python3 behaverify_sequence.py $cur_file create_root --output_file ../models_sequence/$cur_file.smv --overwrite
-    
-    python3 behaverify_path.py $cur_file create_root --output_file ../models_path/$cur_file.smv --overwrite
-    
-    python3 behaverify_double_path.py $cur_file create_root --output_file ../models_double_path/$cur_file.smv --overwrite
+    for cur_version in ${versions[@]}; do
+	python3 behaverify_$cur_version.py $cur_file create_root --root_args False --output_file ../models_$cur_version/$cur_file.smv --overwrite
+    done
 done
-
