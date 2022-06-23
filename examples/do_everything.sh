f60126d4b20ffa0f51f094b89058168e94da0e35
serene <bserbinowski@gmail.com>
2022-06-22 23:18:46 -0500

    ready for tests

--- examples/do_everything.sh
+++ examples/do_everything.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 
-#./generate_all.sh
+./generate_all.sh
 mkdir -p ./results/gcd/processed_data
 mkdir -p ./results/basic/processed_data
 mkdir -p ./results/blueROV/processed_data
 mkdir -p ./results/auto_example/processed_data
 mkdir -p ./results/robot/processed_data
-#./run_all.sh
-#python3 build_table.py gcd
-#python3 build_table.py basic
-#python3 build_table.py blueROV
-#python3 build_table.py auto_example
-#python3 build_table.py robot
+mkdir -p ./processed_data/gcd
+mkdir -p ./processed_data/basic
+mkdir -p ./processed_data/blueROV
+mkdir -p ./processed_data/auto_example
+mkdir -p ./processed_data/robot
+./run_all.sh
+python3 build_table.py gcd
+python3 build_table.py basic
+python3 build_table.py blueROV
+python3 build_table.py auto_example
+python3 build_table.py robot
--- examples/gcd/gcd/generate.sh
+++ examples/gcd/gcd/generate.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-#files=("serene_uc1")
 files=("gcd_example")
-#files=("example0")
-#files=("example1")
 
-#versions=("double_path_clean" "double_path_cleaner")
-#versions=("total" "leaf")
-versions=("total" "leaf" "BTCompiler" "leaf_no_IVAR" "total_no_IVAR_errorless_unique_child")
+versions=("leaf" "leaf_no_IVAR" "total" "total_no_IVAR_errorless_unique_child")
 
 #gen_mode=2
 gen_mode=0
 
-for cur_file in ${files[@]}; do
-    for cur_version in ${versions[@]}; do
+for cur_version in ${versions[@]}; do
+    mkdir -p ../models_$cur_version
+    for cur_file in ${files[@]}; do
 	if [ $gen_mode -eq 0 ]; then
 	   python3 behaverify_$cur_version.py $cur_file create_root --root_args False --min_val -1 --max_val 100 --no_seperate_variable_modules --blackboard_input_file ../modules_and_specs/$cur_version/$cur_file-blackboard.smv --module_input_file ../modules_and_specs/$cur_version/$cur_file.smv --specs_input_file ../modules_and_specs/$cur_version/$cur_file-specs.smv  --output_file ../models_$cur_version/$cur_file.smv --overwrite
 	else
