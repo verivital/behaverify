0d1df358d277b7f1f02c273914e1eab88333b1c8
serene <bserbinowski@gmail.com>
2022-06-23 11:21:58 -0500

    updated test

--- examples/auto_example/auto_example/generate.sh
+++ examples/auto_example/auto_example/generate.sh
@@ -5,8 +5,19 @@ versions=("BTCompiler" "leaf" "leaf_no_IVAR" "total" "total_no_IVAR_errorless_un
 
 for cur_version in ${versions[@]}; do
     mkdir -p ../models_$cur_version
-    for sel_num in {0..20}; do
+    for sel_num1 in {0..4}; do
+	sel_num=$((5*$sel_num1))
 	for seq_num in {0..20}; do
+	    echo $sel_num $seq_num $cur_version
+	    python3 behaverify_$cur_version.py auto_generate create_root --root_args False $sel_num $seq_num --output_file ../models_$cur_version/sel$sel_num-seq$seq_num.smv --overwrite
+	done
+    done
+done
+
+for cur_version in ${versions[@]}; do
+    for seq_num1 in {0..4}; do
+	seq_num=$((5*$seq_num1))
+	for sel_num in {0..20}; do
 	    echo $sel_num $seq_num $cur_version
 	    python3 behaverify_$cur_version.py auto_generate create_root --root_args False $sel_num $seq_num --output_file ../models_$cur_version/sel$sel_num-seq$seq_num.smv --overwrite
 	done
--- examples/blueROV-run_all.sh
+++ examples/blueROV-run_all.sh
@@ -5,13 +5,13 @@ files=("blueROV_warnings_only" "blueROV_small" "blueROV_full")
 ltls=("_battery" "_emergency" "_home_reached" "_obstacle" "_sensor")
 
 for cur_folder in ${folders[@]}; do
-    timeout_val=1h
-    for ltl in ${ltls[@]}; do
-	for cur_file in ${files[@]}; do
+    timeout_val=12m
+    for cur_file in ${files[@]}; do
+	for ltl in ${ltls[@]}; do
 	    echo $cur_folder $cur_file$ltl
 	    timeout $timeout_val ./run_tests.sh blueROV $cur_folder $cur_file$ltl
 	    if [ $? == 0 ]; then
-		timeout_val=4m
+		timeout_val=12m
 	    fi
 	done
     done
