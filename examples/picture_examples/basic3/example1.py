f5dc0ee743c1e7de7ded9da3a585d7c1b0bbcbcb
serene <bserbinowski@gmail.com>
2022-08-18 17:08:06 -0500

    updates to new tree_parser, smv_writer. version not yet finished

--- examples/picture_examples/basic3/example1.py
+++ examples/picture_examples/basic3/example1.py
@@ -12,6 +12,6 @@ def create_root(print_pic=False):
     seq1.add_children([d2, d3])
     if print_pic:
         py_trees.display.render_dot_tree(sel1, target_directory = '../pictures')#works
-    return seq1
+    return sel1
 
 create_root(True)
