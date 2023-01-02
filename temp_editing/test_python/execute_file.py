e29801f09472507376de2b062485f28f8d202bd8
serene <bserbinowski@gmail.com>
2023-01-02 11:22:56 -0600

    fixed python creator blackboard errors

--- temp_editing/test_python/execute_file.py
+++ temp_editing/test_python/execute_file.py
@@ -8,7 +8,7 @@ class execute(py_trees.behaviour.Behaviour):
     def __init__(self, name):
         super(execute, self).__init__(name)
         self.name = name
-        self.blackboard = py_trees.blackboard.Blackboard()
+        self.blackboard = self.attach_blackboard_client(name = name)
 
     def update(self):
         return_status = random.choice([py_trees.common.Status.SUCCESS])
