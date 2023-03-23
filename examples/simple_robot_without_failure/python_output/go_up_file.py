427e338e667a0a47c97893bfef2a98103f1a0fd5
serene <bserbinowski@gmail.com>
2023-03-23 01:51:30 -0500

    added aut version for comparison

---
+++ examples/simple_robot_without_failure/python_output/go_up_file.py
@@ -0,0 +1,17 @@
+import py_trees
+import math
+import operator
+import random
+import robot
+
+
+class go_up(py_trees.behaviour.Behaviour):
+    def __init__(self, name):
+        super(go_up, self).__init__(name)
+        self.name = name
+        self.blackboard = self.attach_blackboard_client(name = name)
+
+    def update(self):
+        robot.go_y(1)
+        return_status = py_trees.common.Status.SUCCESS
+        return return_status
