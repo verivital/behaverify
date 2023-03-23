a217f587a3a92862f00fc32d61e8b8bba6880e56
serene <bserbinowski@gmail.com>
2023-03-23 01:51:30 -0500

    added aut version for comparison

---
+++ examples/simple_robot_without_failure/python_output/y_too_small_file.py
@@ -0,0 +1,15 @@
+import py_trees
+import math
+import operator
+import random
+class y_too_small(py_trees.behaviour.Behaviour):
+    def __init__(self, name):
+        super(y_too_small, self).__init__(name)
+        self.name = name
+        self.blackboard = self.attach_blackboard_client(name = name)
+        self.blackboard.register_key(key = ('target_y'), access = py_trees.common.Access.READ)
+        self.blackboard.register_key(key = ('y'), access = py_trees.common.Access.READ)
+
+    def update(self):
+        return ((py_trees.common.Status.SUCCESS) if ((self.blackboard.y < self.blackboard.target_y)) else (py_trees.common.Status.FAILURE))
+
