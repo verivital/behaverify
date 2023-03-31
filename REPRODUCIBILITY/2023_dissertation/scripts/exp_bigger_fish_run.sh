cc7d3c7ed81b365f282bd39fda699822b4a88fb3
serene <bserbinowski@gmail.com>
2023-03-31 09:28:03 -0500

    ran tests for bigger fish

--- REPRODUCIBILITY/2023_dissertation/scripts/exp_bigger_fish_run.sh
+++ REPRODUCIBILITY/2023_dissertation/scripts/exp_bigger_fish_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for num in {0..30}; do
+for num in {0..99}; do
     echo $num
     ./run_all.sh ../examples/bigger_fish bigger_fish_parallel_$num
     ./run_all.sh ../examples/bigger_fish bigger_fish_sequence_$num
