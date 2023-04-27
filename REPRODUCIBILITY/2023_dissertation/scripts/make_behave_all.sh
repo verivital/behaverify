68c847e6f6cc26b03a56d490336b3ed126aa1487
serene <bserbinowski@gmail.com>
2023-04-26 22:48:08 -0500

    all versions functioning for the purpose of basic tests (most are super untested and therefore will not work with general stuff like define)

--- REPRODUCIBILITY/2023_dissertation/scripts/make_behave_all.sh
+++ REPRODUCIBILITY/2023_dissertation/scripts/make_behave_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-encodings=("aut_" "aut_s_" "depth_" "" "func_")
+encodings=("aut_" "aut_s_" "depth_" "" "func_" "no_internal_" "s_var_")
 # encodings=("")
 
 for encoding in "${encodings[@]}"; do
