fa17bdec0dc6f51e3430b57c2874036289889061
Serene <bserbinowski@gmail.com>
2023-05-04 11:12:24 -0500

    minor script updates for invar

--- REPRODUCIBILITY/2023_dissertation/scripts/exp_checklist_invar_table.sh
+++ REPRODUCIBILITY/2023_dissertation/scripts/exp_checklist_invar_table.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ./build_table.py --folder_name checklist --file_name checklist_parallel checklist_sequence --minV 1 --maxV 50 --step 1 --xLabel "Number of Checks" --encodings "all" "internal" "core" "aut" "func"
+python3 ./build_table.py --folder_name checklist_invar --file_name checklist_parallel checklist_sequence --minV 1 --maxV 50 --step 1 --xLabel "Number of Checks" --encodings "invar" "internal" "func"
