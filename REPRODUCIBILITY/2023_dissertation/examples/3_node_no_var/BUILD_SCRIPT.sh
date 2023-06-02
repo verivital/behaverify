5e1f4c64d2cea47266e8e7594f5158e928e1f2bc
Serene <bserbinowski@gmail.com>
2023-06-02 12:53:11 -0500

    idk

---
+++ REPRODUCIBILITY/2023_dissertation/examples/3_node_no_var/BUILD_SCRIPT.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dots="../../../../"
+file_name=$1
+rm -r "$file_name"
+mkdir "$file_name"
+python3 "$dots"src/dsl_to_behaverify.py "$dots"metamodel/behaverify.tx "$file_name".tree --output_file ./"$file_name"/"$file_name.smv"
+python3 "$dots"src/dsl_to_pytree.py "$dots"metamodel/behaverify.tx "$file_name".tree ./"$file_name"/ "$file_name"
