49712a024a0bb418e056d8a6f3303b94d7460f4c
Serene Grace <bserbinowski@gmail.com>
2023-06-26 13:26:10 -0500

    docker tweak

--- REPRODUCIBILITY/2023_SEFM/docker_replicate_reuslts.sh
+++ REPRODUCIBILITY/2023_SEFM/docker_replicate_reuslts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker start behaverify
-docker exec behaverify /behaverify/REPRODUCIBILITY/2023_sefm/results_script.sh
+docker exec behaverify /behaverify/REPRODUCIBILITY/2023_SEFM/results_script.sh /behaverify/REPRODUCIBILITY/2023_SEFM/
 docker cp behaverify:/behaverify/REPRODUCIBILITY/2022_SEFM/examples/. ./behaverify_results
--- REPRODUCIBILITY/2023_SEFM/docker_test_install.sh
+++ REPRODUCIBILITY/2023_SEFM/docker_test_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker start behaverify
-docker exec behaverify /behaverify/REPRODUCIBILITY/2023_sefm/minimal_script.sh
-docker exec behaverify /behaverify/REPRODUCIBILITY/2023_sefm/gather_data.sh
+docker exec behaverify /behaverify/REPRODUCIBILITY/2023_SEFM/minimal_script.sh /behaverify/REPRODUCIBILITY/2023_SEFM/
 docker cp behaverify:/behaverify/REPRODUCIBILITY/2022_SEFM/examples/. ./behaverify_test
