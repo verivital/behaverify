12458b78537612026b32af19fe98e3550055474d
Serene Grace <bserbinowski@gmail.com>
2023-06-27 12:34:03 -0500

    minor updates to readme

--- REPRODUCIBILITY/2023_SEFM/docker_replicate_results.sh
+++ REPRODUCIBILITY/2023_SEFM/docker_replicate_results.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+fileLoc=$1
+
 docker start behaverify
 docker exec behaverify /behaverify/REPRODUCIBILITY/2023_SEFM/results_script.sh /behaverify/REPRODUCIBILITY/2023_SEFM/
-docker cp behaverify:/behaverify/REPRODUCIBILITY/2023_SEFM/examples/. ./behaverify_results
+docker cp behaverify:/behaverify/REPRODUCIBILITY/2023_SEFM/examples/. "${fileLoc}behaverify_results"
 docker stop behaverify
--- REPRODUCIBILITY/2023_SEFM/docker_test_install.sh
+++ REPRODUCIBILITY/2023_SEFM/docker_test_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+fileLoc=$1
+
 docker start behaverify
 docker exec behaverify /behaverify/REPRODUCIBILITY/2023_SEFM/minimal_script.sh /behaverify/REPRODUCIBILITY/2023_SEFM/
-docker cp behaverify:/behaverify/REPRODUCIBILITY/2023_SEFM/examples/. ./behaverify_install_test_results
+docker cp behaverify:/behaverify/REPRODUCIBILITY/2023_SEFM/examples/. "${fileLoc}behaverify_install_test_results"
 docker stop behaverify
