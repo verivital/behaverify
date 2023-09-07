dd888563a636f6bd7c950d3754ddf30852b25c32
serene <bserbinowski@gmail.com>
2023-09-07 05:51:26 -0500

    example updates

---
+++ examples/light_controller_v3/python/set_direction_file.py
@@ -0,0 +1,22 @@
+import py_trees
+import math
+import operator
+import random
+import serene_safe_assignment
+
+
+class set_direction(py_trees.behaviour.Behaviour):
+    def __init__(self, name, environment, new_direction):
+        self.new_direction = new_direction
+        super(set_direction, self).__init__(name)
+        self.name = name
+        self.environment = environment
+        self.blackboard = self.attach_blackboard_client(name = name)
+        self.blackboard.register_key(key = ('direction'), access = py_trees.common.Access.WRITE)
+        self.blackboard.register_key(key = ('fairness_counter'), access = py_trees.common.Access.WRITE)
+
+    def update(self):
+        self.blackboard.direction = serene_safe_assignment.direction(self.new_direction)
+        self.blackboard.fairness_counter = serene_safe_assignment.fairness_counter(0)
+        return_status = py_trees.common.Status.SUCCESS
+        return return_status
