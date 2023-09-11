b0d9766c488481ea93a071a7166b2d3dfa98f9f8
Serene <serena.serbinowska@gmail.com>
2023-09-11 17:03:15 -0500

    Delete REPRODUCIBILITY/2024_VMCAI/examples/differential_testing/moved directory

--- REPRODUCIBILITY/2024_VMCAI/examples/differential_testing/moved/array_go_gen_files/t0/app/T0.hs
+++
@@ -1,8 +0,0 @@
-module T0 where
-import BehaviorTreeCore
-import BTreeC2
-import BTreeA4
-bTreeNodeA4 = BTreeNode bTreeFunctionA4 [] 2
-bTreeNodeC2 = BTreeNode bTreeFunctionC2 [] 3
-bTreeNodePOne1 = BTreeNode (parallelCreator successOnOneFailureOne) [bTreeNodeA4, bTreeNodeC2] 1
-bTreeNodeDecSr0 = BTreeNode (decoratorCreator (xISyCreator Success Running)) [bTreeNodePOne1] 0
--- REPRODUCIBILITY/2024_VMCAI/examples/differential_testing/moved/array_go_gen_files/t2/app/T2.hs
+++
@@ -1,4 +0,0 @@
-module T2 where
-import BehaviorTreeCore
-import BTreeC1
-bTreeNodeC1 = BTreeNode bTreeFunctionC1 [] 0
