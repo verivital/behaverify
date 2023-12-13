f89aa8eecdcaa824295ede127d02463b12e57dbd
serene <serene@penguin>
2023-12-13 14:43:57 -0700

    minor nfm updates

--- REPRODUCIBILITY/2024_NFM/scripts/encoding_timing_scripts/exp_ANSR_no_net_run.sh
+++ REPRODUCIBILITY/2024_NFM/scripts/encoding_timing_scripts/exp_ANSR_no_net_run.sh
@@ -10,4 +10,5 @@ exp_name="ANSR_no_net"
 
 for (( num=min_val; num<=max_val; num=(num + step_size) )); do
     ../test_scripts/test_ctl.sh $path_name "${exp_name}_${num}" full_opt
+    ../test_scripts/test_ctl_silent.sh $path_name "${exp_name}_${num}" full_opt
 done
