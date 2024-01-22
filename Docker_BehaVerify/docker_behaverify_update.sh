a77eeb431ee4578ee10db8e5b65190020303be4c
Serene Grace <bserbinowski@gmail.com>
2024-01-22 13:10:57 -0600

    updated Docker BehaVerify

--- Docker_BehaVerify/docker_behaverify_update.sh
+++ Docker_BehaVerify/docker_behaverify_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker start behaverify
-docker exec -w /home/user/behaverify behaverify git checkout
+docker exec -w /home/user/behaverify behaverify git pull
 docker stop behaverify
