c80de911e41fa63afa96fb34051bc170767f6cbb
serene <bserbinowski@gmail.com>
2024-01-30 14:31:00 -0600

    updated Docker_BehaVerify update

--- Docker_BehaVerify/docker_behaverify_update.sh
+++ Docker_BehaVerify/docker_behaverify_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker start behaverify
-docker exec -w '/home/behaverify/behaverify' behaverify git checkout
+docker exec -w '/home/behaverify/behaverify' behaverify git pull
 docker exec behaverify '/home/behaverify/behaverify_venv/bin/python3' -m pip install --upgrade pip
 docker exec behaverify '/home/behaverify/behaverify_venv/bin/python3' -m pip install -r '/home/behaverify/behaverify/Docker_BehaVerify/Additional_Files/requirements.txt'
 docker stop behaverify
