f5ad9752e5a0513e4ef1a7e1b7f98453d2f10391
Serene Grace <bserbinowski@gmail.com>
2024-02-02 16:10:44 -0600

    updated haskell checker

--- side/V2.hs
+++ side/V2.hs
@@ -2,6 +2,7 @@ module V2 where
 import Data.List ( sort )
 import qualified Data.Set as Set
 import qualified Data.Map as Map
+import Data.Maybe
 
 data State = State {
   vInt1 :: Integer
@@ -86,11 +87,25 @@ data DoubleElement =
   | IEqD (Integer -> Double) IntegerElement
 
 
-data LTLElement =
-  LTLGlobally BoolElement
-  | LTLFinally BoolElement
-  | LTLNext BoolElement
-  | LTLUntil BoolElement BoolElement
+-- data LTLElement =
+--   LTLGlobally BoolElement
+--   | LTLFinally BoolElement
+--   | LTLNext BoolElement
+--   | LTLUntil BoolElement BoolElement
+
+data CTLElement =
+  CTLAlwaysNext CTLElement
+  | CTLExistsNext CTLElement
+  | CTLAlwaysFinally CTLElement
+  | CTLExistsFinally CTLElement
+  | CTLAlwaysGlobally CTLElement
+  | CTLExistsGlobally CTLElement
+  | CTLNot CTLElement
+  | CTLAnd CTLElement CTLElement
+  | CTLOr CTLElement CTLElement
+  | CTLImplies CTLElement CTLElement
+  | CTLEqual CTLElement CTLElement
+  | CTLEnd BoolElement
 
 evaluateIntegerElement :: State -> IntegerElement -> Integer
 evaluateIntegerElement _ (IConst val) = val
@@ -114,9 +129,70 @@ evaluateDoubleElement state (IEqD func iEl) = func (evaluateIntegerElement state
 evaluateDoubleElement state (DVar varID) = varIDToDouble state varID
 evaluateDoubleElement _ (DConst val) = val
 
+testFunc :: StateMap -> Set.Set State -> Set.Set State
+testFunc stateMap unexploredFalseStates = (Set.foldr (Set.union . (reversedMapLookup stateMap)) Set.empty unexploredFalseStates)
 
--- evaluateLTLElement :: State -> StateMap -> LTLElement -> Bool
--- evaluateLTLElement startState stateMap (LTLNext bEl) = and ([evaluateBoolElement state bEl | state <- stepStateMap startState 1]) 
+ctlAlwaysFinallyStateSearch :: CTLElement -> StateMap -> Set.Set State -> Set.Set State -> Set.Set State -> Bool
+ctlAlwaysFinallyStateSearch ctlEl stateMap seenStates falseStates statesToExplore
+  | Set.null unexploredStates = False -- This means we haven't reached true and have nothing left to check
+  | Set.foldr ((||) . (flip Set.member falseStates)) False statesToExplore = False -- This means we found a state that we've already visited that didn't evaluate to true, meaning an infinite loop where we don't reach true is possible.
+  | otherwise = conclusion -- This means we still have states to check and we haven't encountered an infinit loop
+  where
+    unexploredStates = Set.difference statesToExplore seenStates -- Make sure to remove any overlap
+    unexploredFalseStates = Set.filter (not . (evaluateCTLElementReorder stateMap ctlEl)) unexploredStates -- Filter so we only have states where the condition is false.
+    conclusion
+      | Set.null unexploredFalseStates = True -- If the condition was true for all the states we were exploring, then we've proved that we always reach true.
+      | otherwise = ctlAlwaysFinallyStateSearch ctlEl stateMap (Set.union seenStates unexploredStates) (Set.union falseStates unexploredFalseStates) (Set.foldr (Set.union . (reversedMapLookup stateMap)) Set.empty unexploredFalseStates) -- more searching required.
+
+ctlExistsFinallyStateSearch :: CTLElement -> StateMap -> Set.Set State -> Set.Set State -> Bool
+ctlExistsFinallyStateSearch ctlEl stateMap seenStates statesToExplore  -- we don't have a falseStates here because the infinite negative loop is possible, only if it's forced.
+  | Set.null unexploredStates = False -- This means we haven't reached true and have nothing left to check
+  | otherwise = conclusion -- This means we still have things to explore.
+  where
+    unexploredStates = Set.difference statesToExplore seenStates  -- make sure to remove any overlap.
+    foundTrue = Set.foldr ((||) . (evaluateCTLElementReorder stateMap ctlEl)) False unexploredStates -- check if the condition holds for any of the new states
+    conclusion
+      | foundTrue = True -- if the condition holds for any of the new states, then we're done.
+      | otherwise = ctlExistsFinallyStateSearch ctlEl stateMap (Set.union seenStates unexploredStates) (Set.foldr (Set.union . (reversedMapLookup stateMap)) Set.empty unexploredStates) -- more searching required.
+
+ctlAlwaysGloballyStateSearch :: CTLElement -> StateMap -> Set.Set State -> Set.Set State -> Bool
+ctlAlwaysGloballyStateSearch ctlEl stateMap seenStates statesToExplore -- we don't have a falseStates because if we find any false states we proved it was false.
+  | Set.null unexploredStates = True -- we ran out of states and found nothing wrong, return true.
+  | otherwise = conclusion
+  where
+    unexploredStates = Set.difference statesToExplore seenStates  -- make sure to remove any overlap.
+    allTrue = Set.foldr ((&&) . (evaluateCTLElementReorder stateMap ctlEl)) True unexploredStates -- check if the condition holds for all of the new states
+    conclusion
+      | not allTrue = False -- at least one of our current states failed, so it didn't hold globally.
+      | otherwise = ctlAlwaysGloballyStateSearch ctlEl stateMap (Set.union seenStates unexploredStates) (Set.foldr (Set.union . (reversedMapLookup stateMap)) Set.empty unexploredStates) -- keep searching
+
+ctlExistsGloballyStateSearch :: CTLElement -> StateMap -> Set.Set State -> Set.Set State -> Bool
+ctlExistsGloballyStateSearch ctlEl stateMap seenStates statesToExplore = not (ctlAlwaysFinallyStateSearch (CTLNot ctlEl) stateMap seenStates Set.empty statesToExplore)
+
+
+evaluateCTLElement :: State -> StateMap -> CTLElement -> Bool
+evaluateCTLElement startState stateMap (CTLAlwaysNext ctlEl) = Set.foldr ((&&) . (evaluateCTLElementReorder stateMap ctlEl)) True (fromJust (Map.lookup startState stateMap))
+evaluateCTLElement startState stateMap (CTLExistsNext ctlEl) = Set.foldr ((||) . (evaluateCTLElementReorder stateMap ctlEl)) False (fromJust (Map.lookup startState stateMap))
+evaluateCTLElement startState stateMap (CTLAlwaysFinally ctlEl) = ctlAlwaysFinallyStateSearch ctlEl stateMap Set.empty Set.empty (Set.singleton startState)
+evaluateCTLElement startState stateMap (CTLExistsFinally ctlEl) = ctlExistsFinallyStateSearch ctlEl stateMap Set.empty (Set.singleton startState)
+evaluateCTLElement startState stateMap (CTLAlwaysGlobally ctlEl) = ctlAlwaysGloballyStateSearch ctlEl stateMap Set.empty (Set.singleton startState)
+evaluateCTLElement startState stateMap (CTLExistsGlobally ctlEl) = ctlExistsGloballyStateSearch ctlEl stateMap Set.empty (Set.singleton startState)
+evaluateCTLElement startState stateMap (CTLNot ctlEl) = not (evaluateCTLElement startState stateMap ctlEl)
+evaluateCTLElement startState stateMap (CTLAnd ctlEl1 ctlEl2) = (evaluateCTLElement startState stateMap ctlEl1) && (evaluateCTLElement startState stateMap ctlEl2) 
+evaluateCTLElement startState stateMap (CTLOr ctlEl1 ctlEl2) = (evaluateCTLElement startState stateMap ctlEl1) || (evaluateCTLElement startState stateMap ctlEl2) 
+evaluateCTLElement startState stateMap (CTLImplies ctlEl1 ctlEl2) = (not (evaluateCTLElement startState stateMap ctlEl1)) || (evaluateCTLElement startState stateMap ctlEl2) 
+evaluateCTLElement startState stateMap (CTLEqual ctlEl1 ctlEl2) = (evaluateCTLElement startState stateMap ctlEl1) == (evaluateCTLElement startState stateMap ctlEl2) 
+evaluateCTLElement startState stateMap (CTLEnd bEl) = evaluateBoolElement startState bEl
+
+evaluateCTLElementReorder :: StateMap -> CTLElement -> State -> Bool
+evaluateCTLElementReorder stateMap ctlEl state = evaluateCTLElement state stateMap ctlEl
+
+reversedMapLookup :: StateMap -> State -> Set.Set State
+reversedMapLookup stateMap state = fromJust (Map.lookup state stateMap)
+
+
+evaluateCTLAllInitial :: Set.Set State -> StateMap -> CTLElement -> Bool
+evaluateCTLAllInitial initialStates stateMap ctlEl = Set.foldr ((&&) . (evaluateCTLElementReorder stateMap ctlEl)) True initialStates
 
 
 ifThenElse :: Bool -> a -> a -> a
@@ -153,8 +229,7 @@ allStates elements seenStates exploreStates
   where
     actualExploreStates = Set.difference exploreStates seenStates
     nextStates = Set.unions (Set.map (nextState elements 0) actualExploreStates)
-    newSeenStates = Set.union exploreStates seenStates
-    reachableStates = allStates elements newSeenStates nextStates
+    reachableStates = allStates elements (Set.union seenStates actualExploreStates) nextStates
 
 
 constructStateMap :: [[Element]] -> [[Element]] -> State -> StateMap
@@ -187,7 +262,11 @@ main =
     --mapM_ (putStrLn . stateToString) initialStates
     --mapM_ (putStrLn . stateToString) reachableStates
     --mapM_ print (checkInvariants invariants reachableStates)
-    mapM_ print (constructStateMap initialElements nextElements defaultState)
+    print stateMap
+    ; print (evaluateCTLAllInitial initialStates stateMap ctlSpec1)
+    --; print (and [evaluateLTLElement state stateMap ltlSpecFalse  | state <- Set.toList initialStates])
+    --; print (and [evaluateLTLElement state stateMap ltlSpecTrue  | state <- Set.toList initialStates])
+    --; mapM_ print [Map.lookup state stateMap  | state <- Set.toList initialStates]
   }
   where
     defaultState = State 0 0 0.0 False False
@@ -195,5 +274,7 @@ main =
     nextElements = [[IntegerElement (BIIEqI (ifThenElse) (IIEqB (<) (IVar 0) (IConst 10)) (IIEqI (+) (IVar 0) (IConst 1)) (IConst 0))]]
     initialStates = nextState initialElements 0 defaultState
     reachableStates = allStates nextElements Set.empty initialStates
+    stateMap = (constructStateMap initialElements nextElements defaultState);
     invariants = [(IIEqB (<=) (IVar 0) (IConst 8)), (IIEqB (>) (IVar 0) (IConst 0)), (IIEqB (>=) (IVar 0) (IConst 0))]
-    
+    ctlSpec1 = CTLAlwaysFinally (CTLEnd (IIEqB (==) (IVar 0) (IConst 9)))
+    ctlSpec1 = CTLAlwaysFinally (CTLEnd (IIEqB (==) (IVar 0) (IConst 9)))
