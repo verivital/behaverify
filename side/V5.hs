4f4271b73eeae106952aaaf501ec17a099b9778d
serene <bserbinowski@gmail.com>
2024-02-13 22:36:00 -0600

    minor haskell changes

--- side/V5.hs
+++ side/V5.hs
@@ -68,6 +68,7 @@ nextStates state = next_func
                 value1_2 = 4
                 value2_1 = (+) varA_val 1
                 varA_vals
+                  | (/=) check_val Failure = Set.singleton varA_val
                   | (==) varA_val 5 = Set.fromList [value1_1, value1_2]
                   | otherwise = Set.fromList [value2_1]
                 result = Set.foldr (Set.union . next_func) Set.empty varA_vals
@@ -84,13 +85,13 @@ nextStates state = next_func
                             next_func varB_val = result
                               where
                                 act_vals
-                                  | check_val /= Failure = Set.singleton act_val
+                                  | check_val /= Failure = Set.singleton Invalid
                                   | otherwise = Set.fromList [Success]
                                 result = Set.foldr (Set.union . next_func) Set.empty act_vals
                                   where
                                     sel_val
-                                      | any ((==) Success) [check_val, act_val] = Success
-                                      | any ((==) Running) [check_val, act_val] = Running
+                                      | elem Success [check_val, act_val] = Success
+                                      | elem Running [check_val, act_val] = Running
                                       | otherwise = Failure
                                     next_func :: NodeStatus -> Set.Set State
                                     next_func act_val = Set.singleton (State varA_val varB_val sel_val check_val act_val)
@@ -133,26 +134,17 @@ ctlAX function state = Set.foldr ((&&) . function) True (fromJust (Map.lookup st
 ctlEX :: (State -> Bool) -> State -> Bool
 ctlEX function state = Set.foldr ((||) . function) False (fromJust (Map.lookup state stateMap))
 
--- ctlAF :: (State -> Bool) -> State -> Bool
--- ctlAF function state = ctlAFStateSearch function Set.empty Set.empty (fromJust (Map.lookup state stateMap))
--- ctlAFStateSearch :: (State -> Bool) -> Set.Set State -> Set.Set State -> Set.Set State -> Bool
--- ctlAFStateSearch function seenStates falseStates statesToExplore
---   | Set.null unexploredStates = False -- This means we haven't reached true and have nothing left to check
---   | Set.foldr ((||) . flip Set.member falseStates) False statesToExplore = False -- This means we found a state that we've already visited that didn't evaluate to true, meaning an infinite loop where we don't reach true is possible.
---   | otherwise = conclusion -- This means we still have states to check and we haven't encountered an infinit loop
---   where
---     unexploredStates = Set.difference statesToExplore seenStates -- Make sure to remove any overlap
---     unexploredFalseStates = Set.filter (not . function) unexploredStates -- Filter so we only have states where the condition is false.
---     conclusion
---       | Set.null unexploredFalseStates = True -- If the condition was true for all the states we were exploring, then we've proved that we always reach true.
---       | otherwise = ctlAFStateSearch function (Set.union seenStates unexploredStates) (Set.union falseStates unexploredFalseStates) (Set.foldr (Set.union . reversedMapLookup stateMap) Set.empty unexploredFalseStates) -- more searching required.
-
 ctlAF :: (State -> Bool) -> State -> Bool
-ctlAF function state = not (ctlEF function state) 
+ctlAF function = ctlAFStateSearch function Set.empty
+ctlAFStateSearch :: (State -> Bool) -> Set.Set State -> State -> Bool
+ctlAFStateSearch function seenStates state
+  | function state = True
+  | Set.member state seenStates = False -- This means we haven't reached true and have nothing left to check
+  | otherwise = Set.foldr ((&&) . ctlAFStateSearch function (Set.insert state seenStates)) True (fromJust (Map.lookup state stateMap)) -- more searching required.
 
 
 ctlEF :: (State -> Bool) -> State -> Bool
-ctlEF function state = ctlEFStateSearch function Set.empty (fromJust (Map.lookup state stateMap))
+ctlEF function state = (||) (function state) (ctlEFStateSearch function (Set.singleton state) (fromJust (Map.lookup state stateMap)))
 ctlEFStateSearch :: (State -> Bool) -> Set.Set State -> Set.Set State -> Bool
 ctlEFStateSearch function seenStates statesToExplore
   | Set.null unexploredStates = False -- This means we haven't reached true and have nothing left to check
@@ -166,7 +158,7 @@ ctlEFStateSearch function seenStates statesToExplore
 
 
 ctlAG :: (State -> Bool) -> State -> Bool
-ctlAG function state = ctlAGStateSearch function Set.empty (fromJust (Map.lookup state stateMap))
+ctlAG function state = (&&) (function state) (ctlAGStateSearch function (Set.singleton state) (fromJust (Map.lookup state stateMap)))
 ctlAGStateSearch :: (State -> Bool) -> Set.Set State -> Set.Set State -> Bool
 ctlAGStateSearch function seenStates statesToExplore
   | Set.null unexploredStates = True -- we ran out of states and found nothing wrong, return true.
@@ -178,14 +170,18 @@ ctlAGStateSearch function seenStates statesToExplore
       | not allTrue = False -- at least one of our current states failed, so it didn't hold globally.
       | otherwise = ctlAGStateSearch function (Set.union seenStates unexploredStates) (Set.foldr (Set.union . reversedMapLookup stateMap) Set.empty unexploredStates) -- keep searching
 
+
 ctlEG :: (State -> Bool) -> State -> Bool
-ctlEG function state = ctlEGStateSearch function Set.empty (fromJust (Map.lookup state stateMap))
-ctlEGStateSearch :: (State -> Bool) -> Set.Set State -> Set.Set State -> Bool
-ctlEGStateSearch function seenStates statesToExplore = not (ctlAFStateSearch (not . function) seenStates Set.empty statesToExplore)
+ctlEG function = ctlEGStateSearch function Set.empty
+ctlEGStateSearch :: (State -> Bool) -> Set.Set State -> State -> Bool
+ctlEGStateSearch function seenStates state
+  | not (function state) = False -- it doesn't hold for the state, so it's not true.
+  | Set.member state seenStates = True -- we've looped!
+  | otherwise = Set.foldr ((||) . ctlEGStateSearch function (Set.insert state seenStates)) False (fromJust (Map.lookup state stateMap)) -- more searching required.
 
 
 ctlAU :: (State -> Bool) -> (State -> Bool) -> State -> Bool
-ctlAU function_hold function_release state = ctlAUStateSearch function_hold function_release Set.empty (fromJust (Map.lookup state stateMap))
+ctlAU function_hold function_release state = (||) (function_release state) ((&&) (function_hold state) (ctlAUStateSearch function_hold function_release (Set.singleton state) (fromJust (Map.lookup state stateMap))))
 ctlAUStateSearch :: (State -> Bool) -> (State -> Bool) -> Set.Set State -> Set.Set State -> Bool
 ctlAUStateSearch function_hold function_release seenStates statesToExplore
   | Set.null unexploredStates = False -- we ran out of states and at least one path didn't terminate with the release.
@@ -200,7 +196,7 @@ ctlAUStateSearch function_hold function_release seenStates statesToExplore
 
 
 ctlEU :: (State -> Bool) -> (State -> Bool) -> State -> Bool
-ctlEU function_hold function_release state = ctlEUStateSearch function_hold function_release Set.empty (fromJust (Map.lookup state stateMap))
+ctlEU function_hold function_release state = (||) (function_release state) ((&&) (function_hold state) (ctlEUStateSearch function_hold function_release (Set.singleton state) (fromJust (Map.lookup state stateMap))))
 ctlEUStateSearch :: (State -> Bool) -> (State -> Bool) -> Set.Set State -> Set.Set State -> Bool
 ctlEUStateSearch function_hold function_release seenStates statesToExplore
   | Set.null unexploredStates = False -- we ran out of states and no path terminated with the release.
@@ -208,14 +204,14 @@ ctlEUStateSearch function_hold function_release seenStates statesToExplore
   where
     unexploredStates = Set.difference statesToExplore seenStates  -- make sure to remove any overlap.
     releaseFound = Set.foldr ((||) . function_release) False unexploredStates -- check if the condition holds for all of the not released states
-    holdingStates = Set.filter (function_hold) unexploredStates -- Filter so we only have states where the release condition is false.
+    holdingStates = Set.filter function_hold unexploredStates -- Filter so we only have states where the release condition is false.
     conclusion
       | releaseFound = True -- we managed to reach the release
       | otherwise = ctlEUStateSearch function_hold function_release (Set.union seenStates unexploredStates) (Set.foldr (Set.union . reversedMapLookup stateMap) Set.empty holdingStates) -- keep searching
 
 
 ctlAW :: (State -> Bool) -> (State -> Bool) -> State -> Bool
-ctlAW function_hold function_release state = ctlAWStateSearch function_hold function_release Set.empty (fromJust (Map.lookup state stateMap))
+ctlAW function_hold function_release state = (||) (function_release state) ((&&) (function_hold state) (ctlAWStateSearch function_hold function_release Set.empty (fromJust (Map.lookup state stateMap))))
 ctlAWStateSearch :: (State -> Bool) -> (State -> Bool) -> Set.Set State -> Set.Set State -> Bool
 ctlAWStateSearch function_hold function_release seenStates statesToExplore
   | Set.null unexploredStates = True -- we ran out of states and there were no problems
@@ -228,23 +224,14 @@ ctlAWStateSearch function_hold function_release seenStates statesToExplore
       | not allTrue = False -- at least one of our current states failed, so it didn't reach the point of release
       | otherwise = ctlAWStateSearch function_hold function_release (Set.union seenStates unexploredStates) (Set.foldr (Set.union . reversedMapLookup stateMap) Set.empty notReleasedStates) -- keep searching
 
--- This *might* not work. THink about it more.
--- it doesn't. Assume A -> (B, C), B -> C, C -> D, D -> D. Assume function_hold is true for all except D, and function_release fails for all.
--- Then, ctlEW A should be false, but it will return true, because C wil be in true states and then B will visit C.
+
 ctlEW :: (State -> Bool) -> (State -> Bool) -> State -> Bool
-ctlEW function_hold function_release state = ctlEWStateSearch function_hold function_release Set.empty  Set.empty (fromJust (Map.lookup state stateMap))
-ctlEWStateSearch :: (State -> Bool) -> (State -> Bool) -> Set.Set State -> Set.Set State -> Set.Set State -> Bool
-ctlEWStateSearch function_hold function_release seenStates trueStates statesToExplore
-  | Set.foldr ((||) . flip Set.member trueStates) False statesToExplore = True -- This means we found a state that we've already visited that evaluated to true, meaning an infinite loop where we just hold is possible.
-  | Set.null unexploredStates = False -- we ran out of states, didn't loop, and didn't find release.
-  | otherwise = conclusion
-  where
-    unexploredStates = Set.difference statesToExplore seenStates  -- make sure to remove any overlap.
-    releaseFound = Set.foldr ((||) . function_release) False unexploredStates -- check if the condition holds for all of the not released states
-    holdingStates = Set.filter (function_hold) unexploredStates -- Filter so we only have states where the release condition is false.
-    conclusion
-      | releaseFound = True -- we managed to reach the release
-      | otherwise = ctlEWStateSearch function_hold function_release (Set.union seenStates unexploredStates) (Set.union trueStates holdingStates) (Set.foldr (Set.union . reversedMapLookup stateMap) Set.empty holdingStates) -- keep searching
+ctlEW function_hold function_release = ctlEWStateSearch function_hold function_release Set.empty
+ctlEWStateSearch :: (State -> Bool) -> (State -> Bool) -> Set.Set State -> State -> Bool
+ctlEWStateSearch function_hold function_release seenStates state
+  | function_release state = True
+  | not (function_hold state) = False
+  | otherwise = Set.foldr ((||) . ctlEWStateSearch function_hold function_release (Set.insert state seenStates)) False (fromJust (Map.lookup state stateMap)) -- more searching required.
 
 
 ctl1 :: Bool
@@ -269,7 +256,7 @@ ctl4 :: Bool
 ctl4 = Set.foldr ((&&) . ctl_func) True initialStates
   where
     ctl_func :: State -> Bool
-    ctl_func state = (ctlAG (\state -> (||) (ctlAF (\state -> (>) (varB state) (varA state)) state) ((==) (varB state) 0)) state)
+    ctl_func state = ctlAG (\state -> (||) (ctlAF (\state -> (>) (varB state) (varA state)) state) ((==) (varB state) 0)) state
 
     
 
@@ -277,16 +264,32 @@ ctl5 :: Bool
 ctl5 = Set.foldr ((&&) . ctl_func) True initialStates
   where
     ctl_func :: State -> Bool
-    ctl_func state = ctlAW (\state -> (==) (varB state) 0) (\state -> (ctlAF (\state -> (>) (varB state) (varA state)) state)) state
+    ctl_func state = ctlAW (\state -> (==) (varB state) 0) (\state -> (ctlAF (\state -> (<) (varA state) (varB state)) state)) state
+
+
+ctl6 :: Bool
+ctl6 = Set.foldr ((&&) . ctl_func) True initialStates
+  where
+    ctl_func :: State -> Bool
+    ctl_func state = ctlAW (\state -> (==) (varB state) 0) (\state -> (>) (varB state) 0) state
+
+ctl7 ::Bool
+ctl7 = Set.foldr ((&&) .ctl_func) True initialStates
+  where
+    ctl_func :: State -> Bool
+    ctl_func state = ctlAF (\state -> (>) (varA state) 1) state
 
 main :: IO ()
 main =
   do {
+    -- print reachableStates
+    -- print stateMap
     print initialStates
-    ; print reachableStates
     ; print ctl1
     ; print ctl2
     ; print ctl3
     ; print ctl4
     ; print ctl5
+    ; print ctl6
+    ; print ctl7
   }
