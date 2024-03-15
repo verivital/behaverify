79fa4ff711de885f1a12ea4478560637bd9624a3
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-03-15 14:19:43 -0500

    minor updates

--- examples/ANSR_behaverify_A_star_net/a_star_files/table_to_training.py
+++ examples/ANSR_behaverify_A_star_net/a_star_files/table_to_training.py
@@ -2,7 +2,7 @@ import re
 import os
 import sys
 
-def convert_table_to_training_data(input_path, output_path_input, output_path_target):
+def convert_table_to_training_data(input_path, output_path_input, output_path_target, min_x, min_y, max_x, max_y):
     input_lines = ['inputs = [' + os.linesep]
     target_lines = ['targets = [' + os.linesep]
     target_code = {
@@ -12,8 +12,8 @@ def convert_table_to_training_data(input_path, output_path_input, output_path_ta
         'down' : '[0.0, 0.0, 0.0, 1.0, 0.0]',
         'no_action' : '[0.0, 0.0, 0.0, 0.0, 1.0]'
     }
-    all_min_vals = [None, None, None, None]
-    all_max_vals = [None, None, None, None]
+    # all_min_vals = [None, None, None, None]
+    # all_max_vals = [None, None, None, None]
     input_code = {
         'drone_x' : 0,
         'drone_y' : 1,
@@ -46,9 +46,9 @@ def convert_table_to_training_data(input_path, output_path_input, output_path_ta
                 max_vals[cur_code] = int(split_case[index + 5].strip())
                 index = index + 6
                 continue
-            for index in range(4):
-                all_min_vals[index] = min_vals[index] if all_min_vals[index] is None else min(all_min_vals[index], min_vals[index])
-                all_max_vals[index] = max_vals[index] if all_max_vals[index] is None else max(all_max_vals[index], max_vals[index])
+            # for index in range(4):
+            #     all_min_vals[index] = min_vals[index] if all_min_vals[index] is None else min(all_min_vals[index], min_vals[index])
+            #     all_max_vals[index] = max_vals[index] if all_max_vals[index] is None else max(all_max_vals[index], max_vals[index])
             for s_x in range(min_vals[0], max_vals[0] + 1):
                 for s_y in range(min_vals[1], max_vals[1] + 1):
                     for e_x in range(min_vals[2], max_vals[2] + 1):
@@ -58,14 +58,21 @@ def convert_table_to_training_data(input_path, output_path_input, output_path_ta
                             target_lines.append('    ' + result + ',' + os.linesep)
                             if len(seen_values) % 1000 == 0:
                                 print('iteration: ' + str(len(seen_values)))
+    print(len(seen_values))
     result = target_code['no_action']
-    for s_x in range(all_min_vals[0], all_max_vals[0] + 1):
-        for s_y in range(all_min_vals[1], all_max_vals[1] + 1):
-            for e_x in range(all_min_vals[2], all_max_vals[2] + 1):
-                for e_y in range(all_min_vals[3], all_max_vals[3] + 1):
+    # for s_x in range(all_min_vals[0], all_max_vals[0] + 1):
+    #     for s_y in range(all_min_vals[1], all_max_vals[1] + 1):
+    #         for e_x in range(all_min_vals[2], all_max_vals[2] + 1):
+    #             for e_y in range(all_min_vals[3], all_max_vals[3] + 1):
+    for s_x in range(min_x, max_x + 1):
+        for s_y in range(min_y, max_y + 1):
+            for e_x in range(min_x, max_x + 1):
+                for e_y in range(min_y, max_y + 1):
                     if (s_x, s_y, e_x, e_y) not in seen_values:
+                        seen_values.add((s_x, s_y, e_x, e_y))
                         input_lines.append('    ['+ str(float(s_x)) + ', ' + str(float(s_y)) + ', ' + str(float(e_x)) + ', ' + str(float(e_y)) + '],' + os.linesep)
                         target_lines.append('    ' + result + ',' + os.linesep)
+    print(len(seen_values))
     input_lines.append(']' + os.linesep)
     target_lines.append(']' + os.linesep)
     with open(output_path_input, 'w', encoding = 'utf-8') as output_file:
@@ -74,4 +81,4 @@ def convert_table_to_training_data(input_path, output_path_input, output_path_ta
         output_file.writelines(target_lines)
 
 if __name__ == '__main__':
-    convert_table_to_training_data(sys.argv[1], sys.argv[2], sys.argv[3])
+    convert_table_to_training_data(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6]), int(sys.argv[7]))
