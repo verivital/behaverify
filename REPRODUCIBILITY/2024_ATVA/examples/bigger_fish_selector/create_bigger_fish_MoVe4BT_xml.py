d9f2fb1f1484a855d098d01e3c57e55b3aef32f8
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-04-23 23:52:15 -0500

    minor update

--- REPRODUCIBILITY/2024_ATVA/examples/bigger_fish_selector/create_bigger_fish_MoVe4BT_xml.py
+++ REPRODUCIBILITY/2024_ATVA/examples/bigger_fish_selector/create_bigger_fish_MoVe4BT_xml.py
@@ -5,11 +5,11 @@ def indent(n):
     return ' '*(4*n)
 
 def create_condition(indent_level, x):
-    return indent(indent_level) + '<Condition ID="Is' + str(x) + '" failure_guard="bf&lt;' + str(x) + ' || bf&gt;' + str(x) + '" name="Is' + str(x) + '" success_guard="bf==' + str(x) + '"/>' + os.linesep
+    return indent(indent_level) + '<Condition ID="IIs' + str(x) + '" failure_guard="bf&lt;' + str(x) + ' || bf&gt;' + str(x) + '" name="NIs' + str(x) + '" success_guard="bf==' + str(x) + '"/>' + os.linesep
 
 def create_subtree_selector(indent_level, x):
     return (
-        indent(indent_level) + '<Fallback name="Selector' + str(x) + '">' + os.linesep
+        indent(indent_level) + '<Fallback name="F' + str(x) + '">' + os.linesep
         + ''.join(
             [
                 (create_condition(indent_level + 1, y))
@@ -34,31 +34,33 @@ def create_subtree_selector(indent_level, x):
 def create_fish_selector(x):
     return (
         '<?xml version="1.0"?>' + os.linesep
-        + '<root _reach_goal="" env="True" expected_execution_time="" global_vars="var bf=0;" main_tree_to_execute="BehaviorTree" specification="#assert BehaviorTree |= F(MoVe4BTCheck_s);">' + os.linesep
+        + '<root _reach_goal="" env="True" expected_execution_time="" global_vars="var bf=0;" main_tree_to_execute="BehaviorTree" specification="#assert BehaviorTree |= F(IC_s);">' + os.linesep
         + indent(1) + '<!-- ////////// -->' + os.linesep
         + indent(1) + '<BehaviorTree ID="BehaviorTree">' + os.linesep
-        + indent(2) + '<Fallback name="MoVe4BTSelector">' + os.linesep
-        + indent(3) + '<Sequence name="BiggerFishSequence">' + os.linesep
+        + indent(2) + '<Fallback name="FF">' + os.linesep
+        + indent(3) + '<Sequence name="SS">' + os.linesep
         + create_subtree_selector(4, x)
-        + indent(4) + '<Action ID="BiggerFish" success_guard="bf &gt;= 0" name="BiggerFish" success_program="bf=bf+1"/>' + os.linesep
+        + indent(4) + '<Action ID="IBF" failure_guard="0==1" name="NBF" success_guard="bf &gt;= 0" success_program="bf=bf+1" running_guard="0==1"/>' + os.linesep
         + indent(3) + '</Sequence>' + os.linesep
-        + indent(3) + '<Condition ID="MoVe4BTCheck" failure_guard="bf&lt;' + str(x + 1) + ' || bf&gt;' + str(x + 1) + '" name="MoVe4BTCheck" success_guard="bf==' + str(x + 1) + '"/>' + os.linesep
+        + indent(3) + '<Condition ID="IC" failure_guard="bf&lt;' + str(x + 1) + ' || bf&gt;' + str(x + 1) + '" name="NC" success_guard="bf==' + str(x + 1) + '"/>' + os.linesep
         + indent(2) + '</Fallback>' + os.linesep
         + indent(1) + '</BehaviorTree>' + os.linesep
         + indent(1) + '<!-- ////////// -->' + os.linesep
         + indent(1) + '<TreeNodesModel>' + os.linesep
-        + indent(2) + '<Action ID="BiggerFish">' + os.linesep
+        + indent(2) + '<Action ID="IBF">' + os.linesep
         + indent(3) + '<inout_port name="success_guard"/>' + os.linesep
         + indent(3) + '<inout_port name="success_program"/>' + os.linesep
+        + indent(3) + '<inout_port name="failure_guard"/>' + os.linesep
+        + indent(3) + '<inout_port name="running_guard"/>' + os.linesep
         + indent(2) + '</Action>' + os.linesep
-        + indent(2) + '<Condition ID="MoVe4BTCheck">' + os.linesep
+        + indent(2) + '<Condition ID="IC">' + os.linesep
         + indent(3) + '<inout_port name="failure_guard"/>' + os.linesep
         + indent(3) + '<inout_port name="success_guard"/>' + os.linesep
         + indent(2) + '</Condition>' + os.linesep
         + ''.join(
             [
                 (
-                    indent(2) + '<Condition ID="Is' + str(cur_x) + '">' + os.linesep
+                    indent(2) + '<Condition ID="IIs' + str(cur_x) + '">' + os.linesep
                     + indent(3) + '<inout_port name="failure_guard"/>' + os.linesep
                     + indent(3) + '<inout_port name="success_guard"/>' + os.linesep
                     + indent(2) + '</Condition>' + os.linesep
