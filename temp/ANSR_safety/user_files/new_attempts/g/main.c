e1ee28df2efec48d0fce55a5f9cdd2637bba0945
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-05-16 22:07:58 -0500

    more nurv

---
+++ temp/ANSR_safety/user_files/new_attempts/g/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "monitor.h"
+
+
+int main() {
+  //int dirs[5] = {left, right, up, down, no_action};
+  int dirs[5] = {0, 1, 2, 3, 4};
+  for (int sx = 0; sx <= 2; sx++){
+    for (int sy = 0; sy <= 2; sy++){
+      for (int ex = 0; ex <= 2; ex++){
+	for (int ey = 0; ey <= 2; ey++){
+	  for (int dir_loc = 0; dir_loc < 5; dir_loc++){
+	    for (int speed = 1; speed <= 2; speed++){
+	      int ptr = 1;
+	      monitor_input_t start_state;// = {sx, sy, speed, dir};
+	      start_state.system_drone_x = sx;
+	      start_state.system_drone_y = sy;
+	      start_state.system_drone_speed = speed;
+	      start_state.system_going_left = dirs[dir_loc] == 0;
+	      start_state.system_going_right = dirs[dir_loc] == 1;
+	      start_state.system_going_down = dirs[dir_loc] == 2;
+	      start_state.system_going_up = dirs[dir_loc] == 3;
+	      monitor_input_t end_state;// = {sx, sy, speed, dir};
+	      end_state.system_drone_x = ex;
+	      end_state.system_drone_y = ey;
+	      end_state.system_drone_speed = speed;
+	      end_state.system_going_left = dirs[dir_loc] == 0;
+	      end_state.system_going_right = dirs[dir_loc] == 1;
+	      end_state.system_going_down = dirs[dir_loc] == 2;
+	      end_state.system_going_up = dirs[dir_loc] == 3;
+	      RV_value result_start = monitor_scalar(&start_state, NULL, 1, &ptr);
+	      RV_value result_end = monitor_scalar(&end_state, NULL, 0, &ptr);
+	      if (result_end != 3){
+		printf("from (%d, %d) to (%d, %d) via (%d, %d) -> (%d, %d)\n", sx, sy, ex, ey, dir_loc, speed, result_start, result_end);
+	      }
+	    }
+	  }
+	}
+      }
+    }
+  }
+  return 0;
+}
