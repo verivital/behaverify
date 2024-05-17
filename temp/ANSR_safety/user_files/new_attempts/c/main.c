375a14562f247a74c0256e19e460542f104120de
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-05-16 22:07:58 -0500

    more nurv

---
+++ temp/ANSR_safety/user_files/new_attempts/c/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "monitor.h"
+
+
+int main() {
+  int ptr = 1;
+
+  int sx = 6;
+  int sy = 6;
+  int speed = 1;
+  int dir = left;
+  monitor_input_t start_state;// = {sx, sy, speed, dir};
+  start_state.system_drone_x = sx;
+  start_state.system_drone_y = sy;
+  start_state.system_drone_speed = speed;
+  start_state.system_current_action = dir;
+  RV_value ini_res = monitor_scalar(&start_state, NULL, 1, &ptr); // Call monitor_ex function from monitor.h
+  printf("initial %d\n", ini_res);
+  printf("-----------------------------------------------\n");
+  for (int i = 0; i <= 6; i++){
+    RV_value result = monitor_scalar(&start_state, NULL, 1, &ptr); // Call monitor_ex function from monitor.h
+    //printf("xs %d: %d\n", i+1, result);
+    monitor_input_t new_state_x;// = {i, sy, 1, no_action};
+    new_state_x.system_drone_x = i;
+    new_state_x.system_drone_y = sy;
+    new_state_x.system_drone_speed = speed;
+    new_state_x.system_current_action = dir;
+    result = monitor_scalar(&new_state_x, NULL, 0, &ptr); // Call monitor_ex function from monitor.h
+    printf("xn %d: %d\n", i, result);
+  }
+  printf("-----------------------------------------------\n");
+  for (int i = 0; i <= 6; i++){
+    RV_value result = monitor_scalar(&start_state, NULL, 1, &ptr); // Call monitor_ex function from monitor.h
+    //printf("ys %d: %d\n", i+1, result);
+    monitor_input_t new_state_y;// = {sx, i, 1, no_action};
+    new_state_y.system_drone_x = sx;
+    new_state_y.system_drone_y = i;
+    new_state_y.system_drone_speed = speed;
+    new_state_y.system_current_action = dir;
+    result = monitor_scalar(&new_state_y, NULL, 0, &ptr); // Call monitor_ex function from monitor.h
+    printf("yn %d: %d\n", i, result);
+  }
+
+  ptr = 1;
+  // Your loop that calls monitor_ex
+  monitor_input_t states[6] = {
+    {0, 6, 2, right},
+    {2, 6, 2, down},
+    {2, 4, 1, down}, //crash inevitable
+    {2, 3, 1, down}, //crash occurs
+    {2, 4, 1, left}, // reset
+    {1, 4, 1, up}
+  };
+  RV_reset resets[6] = {
+    0,
+    0,
+    0,
+    0,
+    1,
+    0
+  };
+  printf("-----------------------------------------------\n");
+  for (int i = 0; i < 6; i++) {
+    //RV_value result = monitor_ex(&states[i], NULL, resets[i], &ptr); // Call monitor_ex function from monitor.h
+    RV_value result = monitor_scalar(&states[i], NULL, resets[i], &ptr); // Call monitor_ex function from monitor.h
+    printf("Result of monitor_scalar call %d: %d\n", i+1, result);
+  }
+  return 0;
+}
