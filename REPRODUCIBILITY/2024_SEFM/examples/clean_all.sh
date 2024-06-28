35d3b36c0af4ce4ce00d622c8fb710159917bcee
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-06-28 03:21:12 -0500

    Fixed clean

--- REPRODUCIBILITY/2024_SEFM/examples/clean_all.sh
+++ REPRODUCIBILITY/2024_SEFM/examples/clean_all.sh
@@ -2,8 +2,8 @@
 folders=('ISR' 'bigger_fish' 'simple_robot')
 sub_folders=('results' 'smv' 'tree' 'processed_data' 'xml')
 
-for folder in "${folder[@]}"; do
-    for sub_folder in "${sub_folder[@]}"; do
+for folder in "${folders[@]}"; do
+    for sub_folder in "${sub_folders[@]}"; do
 	if test -e ./$folder/$sub_folder; then 
 	    rm -r ./$folder/$sub_folder
 	fi
