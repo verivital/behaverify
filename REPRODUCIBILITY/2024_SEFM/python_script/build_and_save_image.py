678169a14ff58ff533d7e24b6bd9e879302ada6e
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-07-16 20:00:07 -0500

    Updated SEFM reproducibility

--- REPRODUCIBILITY/2024_SEFM/python_script/build_and_save_image.py
+++ REPRODUCIBILITY/2024_SEFM/python_script/build_and_save_image.py
@@ -1,19 +1,19 @@
 import argparse
 import tarfile
 import docker
-from docker_util import IMAGE_NAME, CONTAINER_NAME
+from docker_util import IMAGE_NAME
 
 def create_image_and_container(dockerfile_path):
     '''creates behaverify_img (Docker Image) and behaverify (Docker Container)'''
     client = docker.from_env()
-    print('Start: Building new BehaVerify Image.')
+    print('Start: Building image: ' + IMAGE_NAME)
     (behaverify_img, logs) = client.images.build(path = dockerfile_path, tag = IMAGE_NAME + ':latest')
-    print('End: Building new BehaVerify Image.')
-    print('Start: Saving BehaVerify Image.')
+    print('End: Building image: ' + IMAGE_NAME)
+    print('Start: Saving image: ' + IMAGE_NAME)
     with open('./DockerImage.tar', 'wb') as destination_file:
         for chunk in behaverify_img.save(named = True):
             destination_file.write(chunk)
-    print('End: Saving new BehaVerify Container.')
+    print('End: Saving image: ' + IMAGE_NAME)
     return
 
 if __name__ == '__main__':
