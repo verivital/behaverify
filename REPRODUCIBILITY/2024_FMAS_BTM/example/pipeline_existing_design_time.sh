a84ba52aca2a9c1ca3aaf589ffabc6c8696f420d
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-10-09 19:00:39 -0400

    removed liveness from running

--- REPRODUCIBILITY/2024_FMAS_BTM/example/pipeline_existing_design_time.sh
+++ REPRODUCIBILITY/2024_FMAS_BTM/example/pipeline_existing_design_time.sh
@@ -49,20 +49,20 @@ for category in "${categories[@]}"; do
     done
 done
 
-for category in "${categories[@]}"; do
-    echo $category
-    for folder in "${category}"/*; do
-	if [ -f "${folder}" ]; then
-	    continue
-	fi
-	echo "----------------------------"
-	echo $folder
-	tail_end=$(basename "${folder}")
-	echo $tail_end
-	max_val=$(echo $tail_end | cut -d'_' -f1)
-	echo $max_val
-	echo "running ltl"
-	../nuXmv -source ../scripts/nuxmv_commands/command_all_ltl "${category}/${tail_end}/design_time/smv/ANSRtFilled_${tail_end}.smv" > "${category}/${tail_end}/design_time/ltl.txt" 
-	sleep 3
-    done
-done
+# for category in "${categories[@]}"; do
+#     echo $category
+#     for folder in "${category}"/*; do
+# 	if [ -f "${folder}" ]; then
+# 	    continue
+# 	fi
+# 	echo "----------------------------"
+# 	echo $folder
+# 	tail_end=$(basename "${folder}")
+# 	echo $tail_end
+# 	max_val=$(echo $tail_end | cut -d'_' -f1)
+# 	echo $max_val
+# 	echo "running ltl"
+# 	../nuXmv -source ../scripts/nuxmv_commands/command_all_ltl "${category}/${tail_end}/design_time/smv/ANSRtFilled_${tail_end}.smv" > "${category}/${tail_end}/design_time/ltl.txt" 
+# 	sleep 3
+#     done
+# done
