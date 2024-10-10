a395e1705ee22a8e91633d73b2523f1e37d10e0f
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-10-09 21:30:49 -0400

    fixed typo

--- REPRODUCIBILITY/2024_FMAS_BTM/example/make_graphs.sh
+++ REPRODUCIBILITY/2024_FMAS_BTM/example/make_graphs.sh
@@ -13,7 +13,7 @@ fi
 mkdir "./images"
 
 $python_results graph_sizes.py ./dense_fixed/collision_behaverify_sizes.txt ./dense_fixed/collision_copilot_sizes.txt ./images/file_sizes_dense_fixed
-$python_results graph_sizes.py ./sparse_random/collision_behaverify_sizes.txt ./sparse_random/collision_copilot_sizes.txt ./images/file_sizes/sparse_random
+$python_results graph_sizes.py ./sparse_random/collision_behaverify_sizes.txt ./sparse_random/collision_copilot_sizes.txt ./images/file_sizes_sparse_random
 
 $python_results graph_timing.py ./dense_fixed/timing_behaverify.txt ./dense_fixed/timing_copilot.txt ./dense_fixed/timing_monitorless.txt ./images/timing_dense_fixed
 $python_results graph_timing.py ./sparse_random/timing_behaverify.txt ./sparse_random/timing_copilot.txt ./sparse_random/timing_monitorless.txt ./images/timing_sparse_random
