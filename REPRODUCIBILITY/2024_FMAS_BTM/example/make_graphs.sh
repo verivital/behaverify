224b80c7ad48cca8db6add4d56a71fc76ce4a77f
Serena Serafina Serbinowska <serena.serbinowska@gmail.com>
2024-10-09 20:26:52 -0400

    cleaned up repo

--- REPRODUCIBILITY/2024_FMAS_BTM/example/make_graphs.sh
+++ REPRODUCIBILITY/2024_FMAS_BTM/example/make_graphs.sh
@@ -7,11 +7,11 @@ if [[ $# -ge 1 ]]; then
     python_results=$1
 fi
 
-$python_results graph_sizes.py ./dense_fixed/collision_behaverify_sizes.txt ./dense_fixed/collision_copilot_sizes.txt ./collision_sizes_dense_fixed
-$python_results graph_sizes.py ./sparse_random/collision_behaverify_sizes.txt ./sparse_random/collision_copilot_sizes.txt ./collision_sizes_sparse_random
+$python_results graph_sizes.py ./dense_fixed/collision_behaverify_sizes.txt ./dense_fixed/collision_copilot_sizes.txt ./dense_fixed/file_sizes
+$python_results graph_sizes.py ./sparse_random/collision_behaverify_sizes.txt ./sparse_random/collision_copilot_sizes.txt ./sparse_random/file_sizes
 
-$python_results graph_timing.py ./dense_fixed/timing_behaverify.txt ./dense_fixed/timing_copilot.txt ./dense_fixed/timing_monitorless.txt ./dense_fixed/timing_nurv.txt ./timing_dense_fixed
-$python_results graph_timing.py ./sparse_random/timing_behaverify.txt ./sparse_random/timing_copilot.txt ./sparse_random/timing_monitorless.txt ./sparse_random/timing_nurv.txt ./timing_sparse_random
+$python_results graph_timing.py ./dense_fixed/timing_behaverify.txt ./dense_fixed/timing_copilot.txt ./dense_fixed/timing_monitorless.txt ./timing_dense_fixed
+$python_results graph_timing.py ./sparse_random/timing_behaverify.txt ./sparse_random/timing_copilot.txt ./sparse_random/timing_monitorless.txt ./timing_sparse_random
 
 $python_results graph_timing_design_time.py ./dense_fixed/timing_design_time.txt ./sparse_random/timing_design_time.txt ./timing_design_time
 
